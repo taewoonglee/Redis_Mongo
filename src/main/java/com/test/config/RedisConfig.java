fba870b32143d51cce8a7aaf1ccf99c90b400376
Amy <wlgpek90@naver.com>
2023-07-03 13:53:38 +0900

    redis

--- src/main/java/com/test/config/RedisConfig.java
+++ src/main/java/com/test/config/RedisConfig.java
@@ -3,34 +3,38 @@ package com.test.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+
 
     @Configuration
     public class RedisConfig {
-        String host = "127.0.0.1"; //로컬서버
-        int port = 6379;   //포트
-        int timeout = 3000;
-        int db = 0;
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        JedisPool pool = new JedisPool(jedisPoolConfig,host,port,timeout,"taewoonglee",db);
-        Jedis jedis = pool.getResource();
         @Bean
         public RedisConnectionFactory redisConnectionFactory() {
-            return new LettuceConnectionFactory();  // Lettuce 사용
+            RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+            redisStandaloneConfiguration.setPassword("admin"); //redis에 비밀번호가 설정 되어 있는 경우 설정해주면 됩니다.
+            LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+            return lettuceConnectionFactory;
         }
         @Bean
-        public RedisTemplate<String,String> redisTemplate() {
-            RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        public RedisTemplate<?,?> redisTemplate(){
+            RedisTemplate<byte[], byte[]> redisTemplate=new RedisTemplate<>();
             redisTemplate.setConnectionFactory(redisConnectionFactory());
-            redisTemplate.setKeySerializer(new StringRedisSerializer());   // Key: String
-            redisTemplate.setValueSerializer(new StringRedisSerializer());  // Value: 직렬화에 사용할 Object 사용하기
+            redisTemplate.setKeySerializer(new StringRedisSerializer());
+            redisTemplate.setValueSerializer(new StringRedisSerializer());
             return redisTemplate;
+        }
+        @Bean
+        public StringRedisTemplate stringRedisTemplate(){
+            StringRedisTemplate stringRedisTemplate=new StringRedisTemplate();
+            stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+            stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+            stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+            return stringRedisTemplate;
+        }
         }
     }
 
